967b583d5b7ad5362a8d601c48385d9bb75341df
Gianluigi Zanetti <zag@crs4.it>
2018-06-14 13:36:35 +0200

    Added script to build components

---
+++ components/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+BASE_VERSION=0.1
+
+docker build -t crs4/tdm-wrf-base:${BASE_VERSION} base
+
+docker build --build-arg BASE_VERSION=${BASE_VERSION} \
+             -t crs4/tdm-wrf-wps:${BASE_VERSION} wps
+
+docker build --build-arg BASE_VERSION=${BASE_VERSION} \
+             --build-arg CMODE=35 --build-arg NEST=1 \
+             -t crs4/tdm-wrf-arw:${BASE_VERSION} arw
+
