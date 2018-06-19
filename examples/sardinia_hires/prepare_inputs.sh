0cd7873545e4f529fee7b9de1c02232e094631d8
Gianluigi Zanetti <zag@crs4.it>
2018-06-19 20:03:11 +0200

    Added explicit data fetching from NOAA.

--- examples/sardinia_hires/prepare_inputs.sh
+++ examples/sardinia_hires/prepare_inputs.sh
@@ -15,13 +15,19 @@ function get_if_missing {
     fi
 }
 
+#
+YEAR=2018
+MONTH=6
+DAY=18
+HOUR=0
+
 # This is the V3 geog file
 UCAR_PATH="http://www2.mmm.ucar.edu/wrf/src/wps_files"
 GEOG_TGZ="geog_complete.tar.gz"
 GEOG_URL="${UCAR_PATH}/${GEOG_TGZ}"
 
 
-MODEL_DATA_DIR=./model_data
+MODEL_DATA_DIR=model_data
 
 
 # get relevant data
@@ -45,6 +51,11 @@ docker run -it --mount type=bind,src=${PWD}/${WPSPRD_DIR},dst=/WPSRUN \
 # prepare the dynamic boundary conditions
 cp ${SCRIPTS}/run_ungrib  ${WPSPRD_DIR}
 cp ${SCRIPTS}/run_metgrid  ${WPSPRD_DIR}
+
+docker run -it --mount type=bind,src=${PWD}/${WPSPRD_DIR},dst=/WPSRUN \
+       crs4/tdm-wrf-gfs-fetch:0.1 \
+       gfs_fetch --year ${YEAR} --month ${MONTH} --day ${DAY} --hour ${HOUR} \
+                 --target-directory /WPSRUN/${MODEL_DATA_DIR}
 
 pushd ${WPSPRD_DIR}
 python ../../../bin/link_grib ${MODEL_DATA_DIR} .
