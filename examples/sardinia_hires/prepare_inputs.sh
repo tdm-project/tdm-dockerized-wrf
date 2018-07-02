720623cab9fd4c23704921ba7f177dcdaa62d1b0
Gianluigi Zanetti <zag@crs4.it>
2018-07-02 18:24:03 +0200

    Moving to tdm-wrf-tools.

--- examples/sardinia_hires/prepare_inputs.sh
+++ examples/sardinia_hires/prepare_inputs.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+#
+# Expected sequence:
+#
+
+
 set -euo pipefail
 
 WPSPRD_DIR=${1:-WPSPRD_DIR}
@@ -20,6 +25,7 @@ YEAR=2018
 MONTH=6
 DAY=18
 HOUR=0
+END_HOUR=6
 
 # This is the V3 geog file
 UCAR_PATH="http://www2.mmm.ucar.edu/wrf/src/wps_files"
@@ -28,7 +34,6 @@ GEOG_URL="${UCAR_PATH}/${GEOG_TGZ}"
 
 
 MODEL_DATA_DIR=model_data
-
 
 # get relevant data
 get_if_missing ${GEOG_TGZ} ${GEOG_URL}
@@ -40,9 +45,22 @@ mkdir ${WPSPRD_DIR}
 
 tar  xf ${GEOG_TGZ} -C ${WPSPRD_DIR}
 
-cp ${PARAM}/namelist.wps ${WPSPRD_DIR}
+cp ${PARAM}/wrf.yaml ${WPSPRD_DIR}
 cp ${PARAM}/Vtable.GFS ${WPSPRD_DIR}/Vtable
 
+docker run -it --mount type=bind,src=${PWD}/${WPSPRD_DIR},dst=/WPSRUN \
+       crs4/tdm-tools:0.1 wrf_configurator --target WPS \
+       --config /WPSRUN/wrf.yaml --ofile=/WPSRUN/namelist.wps \
+       -D"@base.timespans.start.year=${YEAR}"\
+       -D"@base.timespans.start.month=${MONTH}"\
+       -D"@base.timespans.start.day=${DAY}"\
+       -D"@base.timespans.start.hour=${HOUR}"\
+       -D"@base.timespans.end.year=${YEAR}"\
+       -D"@base.timespans.end.month=${MONTH}"\
+       -D"@base.timespans.end.day=${DAY}"\
+       -D"@base.timespans.end.hour=${END_HOUR}"
+
+exit
 # FIXME move the script in the image, so that it will be automagically run
 cp ${SCRIPTS}/run_geogrid  ${WPSPRD_DIR}
 docker run -it --mount type=bind,src=${PWD}/${WPSPRD_DIR},dst=/WPSRUN \
@@ -53,13 +71,13 @@ cp ${SCRIPTS}/run_ungrib  ${WPSPRD_DIR}
 cp ${SCRIPTS}/run_metgrid  ${WPSPRD_DIR}
 
 docker run -it --mount type=bind,src=${PWD}/${WPSPRD_DIR},dst=/WPSRUN \
-       crs4/tdm-wrf-gfs-fetch:0.1 \
+       crs4/tdm-tools:0.1 \
        gfs_fetch --year ${YEAR} --month ${MONTH} --day ${DAY} --hour ${HOUR} \
                  --target-directory /WPSRUN/${MODEL_DATA_DIR}
 
-pushd ${WPSPRD_DIR}
-python ../../../bin/link_grib ${MODEL_DATA_DIR} .
-popd
+docker run -it --mount type=bind,src=${PWD}/${WPSPRD_DIR},dst=/WPSRUN \
+       crs4/tdm-tools:0.1 \
+       link_grib /WPSRUN/${MODEL_DATA_DIR} /WPSRUN
 
 # FIXME maybe use a ENV variable to control what to run
 docker run -it --mount type=bind,src=${PWD}/${WPSPRD_DIR},dst=/WPSRUN \
