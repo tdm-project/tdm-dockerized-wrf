29c922b301aafdc900a3044023b710c277264670
Gianluigi Zanetti <zag@crs4.it>
2018-07-02 18:49:24 +0200

    Bug fix.

--- components/build.sh
+++ components/build.sh
@@ -13,6 +13,4 @@ docker build --build-arg BASE_VERSION=${BASE_VERSION} \
              --build-arg CMODE=35 --build-arg NEST=1 \
              -t crs4/tdm-wrf-arw:${BASE_VERSION} arw
 
-docker build -t crs4/tdm-wrf-analyze:${BASE_VERSION} analyze
-
 docker build -t crs4/tdm-wrf-tools:${BASE_VERSION} tools
